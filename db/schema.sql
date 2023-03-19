d23f626c6c82cc8aa0eabd83a85ef81169828645
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-03-18 23:11:59 -0400

    created db schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,25 @@
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS roles;
-DROP TABLE IF EXISTS employees;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
 
 CREATE TABLE departments (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  dep_name VARCHAR(30) NOT NULL, 
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  dep_name VARCHAR(30) NOT NULL 
 );
 
 CREATE TABLE roles (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL(10,0),
+  salary DECIMAL,
   department_id INT,
-  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE NULL
+  CONSTRAINT fk_department 
+  FOREIGN KEY (department_id) 
+  REFERENCES departments(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
